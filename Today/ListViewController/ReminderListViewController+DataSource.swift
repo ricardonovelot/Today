293ca3995dc7c4d7963a53879606018dd0b2f31f
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-08-06 17:16:08 -0600

    .

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import UIKit
+
+extension ReminderListViewController{
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewCell, indexPath: IndexPath, id: String){
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+            contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        var doneButtonConfiguration = doneButtonConfiguration(for: reminder)
+        doneButtonConfiguration.tintColor = .todayListCellDoneButtonTint
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = .todayListCellBackground
+        cell.backgroundConfiguration = backgroundConfiguration
+    }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration
+    {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        return UICellAccessory.CustomViewConfiguration(
+            customView: button, placement: .leading(displayed: .always))
+    }
+}
--- Today/Models/Date+Today.swift
+++ Today/Models/Date+Today.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+extension Date{
+    var dayAndTimeText: String{
+        let timeText = formatted(date: .omitted, time: .shortened)
+        if Locale.current.calendar.isDateInToday(self){
+            let timeFormat = NSLocalizedString("Today at %@", comment: "Today at time format string")
+            return String(format: timeFormat, timeText)
+        } else{
+            let dateText = formatted(.dateTime.month(.abbreviated).day())
+            let dateandTimeFormat = NSLocalizedString("%@ at %@", comment: "Date and time format string")
+            return String(format: dateandTimeFormat, dateText, timeText)
+        }
+    }
+    
+    
+    var dayText: String{
+        _ = formatted(date: .abbreviated, time: .omitted)
+        if Locale.current.calendar.isDateInToday(self){
+            return String(NSLocalizedString("Today", comment: "Today due date"))
+        } else {
+            let notTodayDate = formatted(.dateTime.month().day().weekday(.wide))
+            return String(notTodayDate)
+        }
+    }
+}
